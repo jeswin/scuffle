8c8b6ead9389ff444b3cf9a9c4879f037152b2c5
Jeswin <jeswinpk@agilehead.com>
2021-03-04 20:57:20 +0530

    Edits to grouping

--- src/app/components/ItemsByDate.tsx
+++ src/app/components/ItemsByDate.tsx
@@ -24,70 +24,82 @@ export default function ItemsByDate(props: ItemsByDateProps) {
         rerender(args.element);
       }
 
-      const groupedTodos = groupEntitiesByDate<Todo>(
-        [[props.items.todos, (x) => x.createdAt]],
-        { mergePast: true }
+      const groupedTodos = Array.from(
+        groupEntitiesByDate<Todo>([[props.items.todos, (x) => x.createdAt]], {
+          mergePast: true,
+        }).entries()
       );
 
-      const groupedItems = groupEntitiesByDate<ScuffleEntity>([
-        [props.items.bookmarks, (x) => x.createdAt],
-        [props.items.notes, (x) => x.createdAt],
-      ]);
+      const groupedItems = Array.from(
+        groupEntitiesByDate<ScuffleEntity>([
+          [props.items.bookmarks, (x) => x.createdAt],
+          [props.items.notes, (x) => x.createdAt],
+        ]).entries()
+      );
 
       return (
         <div>
-          {groupedTodos.size > 0 ? (
+          {groupedTodos.length > 0 ? (
             <>
               <SectionHeading type="h2">Get these done...</SectionHeading>
               <div className="pb-2 rounded-md">
-                {Array.from(groupedTodos.entries()).map(
-                  ([timeString, items]) => (
-                    <div className="mb-8 last:mb-6">
-                      <SectionHeading type="h3" icon={icons.access_time}>
-                        {timeString}
-                      </SectionHeading>
-                      <ul>
-                        {items.map((todo: Todo) => (
-                          <TodoListItem
-                            key={todo.id}
-                            todo={todo}
-                            onCompleteTodo={onCompleteTodo}
-                          />
-                        ))}
-                      </ul>
-                    </div>
-                  )
-                )}
+                {groupedTodos.map(([timeString, items]) => (
+                  <div className="mb-8 last:mb-6">
+                    <SectionHeading type="h3" icon={icons.access_time}>
+                      {timeString}
+                    </SectionHeading>
+                    <ul>
+                      {items.map((todo: Todo) => (
+                        <TodoListItem
+                          key={todo.id}
+                          todo={todo}
+                          onCompleteTodo={onCompleteTodo}
+                        />
+                      ))}
+                    </ul>
+                  </div>
+                ))}
               </div>
             </>
           ) : (
             <></>
           )}
-
-          <SectionHeading type="h2">Bookmarks, Notes and Files</SectionHeading>
-          {Array.from(groupedItems.entries()).map(([timeString, items]) => (
-            <div className="mb-8">
-              <SectionHeading type="h3" icon={icons.access_time}>
-                {timeString}
+          {groupedItems.length > 0 ? (
+            <>
+              <SectionHeading type="h2">
+                Bookmarks, Notes and Files
               </SectionHeading>
-              <ul>
-                {(items.filter((x) => x.type === "bookmark") as Bookmark[]).map(
-                  (bookmark: Bookmark) => (
-                    <BookmarksListItem
-                      key={bookmark.id}
-                      bookmark={bookmark}
-                      summarize={true}
-                    />
-                  )
-                )}
-                {(items.filter((x) => x.type === "note") as Note[]).map(
-                  (note: Note) => (
-                    <NotesListItem key={note.id} note={note} summarize={true} />
-                  )
-                )}
-              </ul>
-            </div>
-          ))}
+              {groupedItems.map(([timeString, items]) => (
+                <div className="mb-8">
+                  <SectionHeading type="h3" icon={icons.access_time}>
+                    {timeString}
+                  </SectionHeading>
+                  <ul>
+                    {(items.filter(
+                      (x) => x.type === "bookmark"
+                    ) as Bookmark[]).map((bookmark: Bookmark) => (
+                      <BookmarksListItem
+                        key={bookmark.id}
+                        bookmark={bookmark}
+                        summarize={true}
+                      />
+                    ))}
+                    {(items.filter((x) => x.type === "note") as Note[]).map(
+                      (note: Note) => (
+                        <NotesListItem
+                          key={note.id}
+                          note={note}
+                          summarize={true}
+                        />
+                      )
+                    )}
+                  </ul>
+                </div>
+              ))}
+            </>
+          ) : (
+            <></>
+          )}
         </div>
       );
     },
