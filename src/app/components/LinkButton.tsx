7413ba5a3a10603385a13e6032d3b33d31d214e8
Jeswin <jeswinpk@agilehead.com>
2021-03-09 21:41:02 +0530

    Typo fix

--- src/app/components/LinkButton.tsx
+++ src/app/components/LinkButton.tsx
@@ -12,7 +12,7 @@ export default function LinkButton(initialProps: LinkButtonProps) {
       return !props.style || props.style === "button" ? (
         <button
           className={getClassName(
-            "px-1 text-xs rounded border border-gray-300 hover:bg-gray-200$",
+            "px-1 text-xs rounded border border-gray-300 hover:bg-gray-200 focus:outline-none",
             props.className
           )}
         >
